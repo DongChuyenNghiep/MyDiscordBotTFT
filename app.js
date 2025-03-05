4323d46bb109ae6515c0fe309b2cc9625176c04e
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-03-05 16:30:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ app.listen(PORT, () => {
 });
 
 // 🌍 Ping chính server của nó mỗi 10 giây để Render không đưa vào trạng thái ngủ
-const SERVER_URL = `https://mydiscordbot-9v5s.onrender.com/`;
+const SERVER_URL = `https://mydiscordbottft.onrender.com`;
 
 setInterval(() => {
     axios.get(SERVER_URL)
